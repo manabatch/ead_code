d0663979b51598b19d87eaa11e968dad71389652
manabatch <131910695+manabatch@users.noreply.github.com>
2023-05-20 01:33:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.css";
+import Parent from './Parent'
+
+const App = () => {
+return (
+	<div className="App">
+	<Parent/>
+	</div>
+);
+}
+
+export default App;
